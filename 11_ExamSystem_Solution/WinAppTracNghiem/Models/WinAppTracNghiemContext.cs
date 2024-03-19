ff0fe31bc4ff73a52799bab3bad5b3cac9f3ebba
Duy Hai <haitdhe170242@fpt.edu.vn>
2024-03-19 14:33:40 +0700

    fix onConfiguration

--- 11_ExamSystem_Solution/WinAppTracNghiem/Models/WinAppTracNghiemContext.cs
+++ 11_ExamSystem_Solution/WinAppTracNghiem/Models/WinAppTracNghiemContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace WinAppTracNghiem.Models
 {
@@ -29,11 +30,12 @@ namespace WinAppTracNghiem.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local);database=WinAppTracNghiem;uid=sa;pwd=123456;Trust Server Certificate=True");
-            }
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            var strConn = config["ConnectionStrings:MyDatabase"];
+            optionsBuilder.UseSqlServer(strConn);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
